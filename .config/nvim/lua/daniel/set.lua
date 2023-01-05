e805c1733b9280ebbd37457ed67bb2854223421f
Daniel Carrasco <danielcarrasco17@gmail.com>
2023-01-04 22:58:09 -0300

    Actualiza set de vim

--- .config/nvim/lua/daniel/set.lua
+++ .config/nvim/lua/daniel/set.lua
@@ -1,9 +1,10 @@
--- --------------- Interface ---------------"
+-- --------------- Interface ---------------
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.numberwidth = 4
+vim.opt.signcolumn = "yes"
 
--- --------------- Tab stop ---------------"
+-- --------------- Tab stop ---------------
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -11,48 +12,51 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
 
-vim.opt.smartindent = true
 
-vim.opt.wrap = false
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-
--- --------------- Search ---------------"
+-- --------------- Search ---------------
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
 
+
+-- --------------- Visuals ---------------
 vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 100
-
+vim.opt.scrolloff = 6
 vim.opt.colorcolumn = "100"
-
-vim.g.mapleader = " "
-
-
-
 vim.opt.errorbells = false
-vim.opt.history = 50
-vim.opt.autoread = true
--- vim.opt.backspace=indent,eol,start
-
 vim.opt.showbreak = ".."
--- vim.opt.textwidth=0
-vim.opt.foldmethod = "indent"
-vim.opt.foldlevel = 99
+vim.opt.wrap = false
+vim.opt.showmode= false
+vim.opt.showtabline = 1
+vim.opt.cursorline = false
 
+
+-- --------------- File format ---------------
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 vim.opt.fileformat = "unix"
 vim.opt.tags = "./tags,tags,tags.vendor,.git/tags;"
 vim.opt.grepformat = "%f:%l:%m"
+
+
+-- --------------- Others ---------------
+
+vim.opt.isfname:append("@-@")
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.updatetime = 100
+
+vim.g.mapleader = " "
+
+vim.opt.history = 50
+vim.opt.autoread = true
+-- vim.opt.backspace=indent,eol,start
+
+-- vim.opt.textwidth=0
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
 
 -- vim.opt.path+=**
 -- vim.opt.wildmenu
@@ -62,9 +66,6 @@ vim.opt.grepformat = "%f:%l:%m"
 vim.opt.showcmd = true
 vim.opt.title = false
 vim.opt.laststatus = 2
-vim.opt.showmode= false
-vim.opt.showtabline = 1
-vim.opt.cursorline = true
 -- vim.opt.splitbelow = true
 -- vim.opt.splitright = true
 
