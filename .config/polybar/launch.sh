3a5d2f687a2a39e2645772157807a1c64765b4a1
Daniel Carrasco <danielcarrasco17@gmail.com>
2023-04-23 20:44:56 -0400

    Mejoras visuales

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,13 +6,13 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-(sleep 2; polybar --config=$HOME/.config/polybar/config.ini bar) &
-#(sleep 2; polybar tray) &
+# (sleep 2; polybar --config=$HOME/.config/polybar/config.ini bar) &
+# (sleep 2; polybar tray) &
 #
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
     MONITOR=$m polybar --reload bar &
   done
 else
-  polybar --reload example &
+  polybar --reload bar &
 fi
