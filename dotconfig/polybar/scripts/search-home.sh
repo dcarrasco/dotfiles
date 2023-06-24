77e7660e1a44e07f436b3205c6a5b0903e8797f1
Daniel Carrasco <danielcarrasco17@gmail.com>
2023-06-24 17:23:18 -0400

    Cambios en scripts

--- dotconfig/polybar/scripts/search-home.sh
+++ dotconfig/polybar/scripts/search-home.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ROFI_CMD="rofi -dmenu -i -p "Buscar""
+ROFI_CMD="rofi -dmenu -i -p "
 filename=$(fdfind . "/home/daniel/" -t f | sed 's/\/home\/daniel\///g' | $ROFI_CMD)
 
 if [[ ! -z "$filename" ]]; then
--- dotconfig/polybar/scripts/switch-monitor.sh
+++ dotconfig/polybar/scripts/switch-monitor.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
-OPTION_MONITOR_INT="Solo monitor interno"
-OPTION_MONITOR_EXT="Solo monitor externo"
-OPTION_MONITOR_BOTH="Ambos monitores"
+OPTION_MONITOR_INT="󰌢  Solo monitor interno"
+OPTION_MONITOR_EXT="󰍹  Solo monitor externo"
+OPTION_MONITOR_BOTH="󰍺  Ambos monitores"
+OPTION_SET_RESOLUTION="󱋆  Cambiar resolucion"
 
 opciones_un_monitor() {
-    echo "$OPTION_MONITOR_INT\n"
+    echo "$OPTION_MONITOR_INT\n\n$OPTION_SET_RESOLUTION\n"
 }
 
 opciones_dos_monitores() {
-    echo "$OPTION_MONITOR_INT\n$OPTION_MONITOR_EXT\n$OPTION_MONITOR_BOTH\n"
+    echo "$OPTION_MONITOR_INT\n$OPTION_MONITOR_EXT\n$OPTION_MONITOR_BOTH\n\n$OPTION_SET_RESOLUTION\n"
 }
 
 external_monitor_name() {
@@ -42,6 +43,10 @@ case $MODO in
     ;;
     $OPTION_MONITOR_BOTH)
         xrandr --output eDP --primary --auto --rotate normal --output $EXTERNAL_MONITOR --auto --rotate normal --right-of eDP
+        bspc wm -r
+    ;;
+    $OPTION_SET_RESOLUTION)
+        arandr
         bspc wm -r
     ;;
 esac
