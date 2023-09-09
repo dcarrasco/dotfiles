affe43037bb388b8ffac6c3482f4268644d4692f
Daniel Carrasco <danielcarrasco17@gmail.com>
2023-09-09 18:00:34 -0300

    Mejoras rofi

--- dotconfig/rofi/powermenu/powermenu.sh
+++ dotconfig/rofi/powermenu/powermenu.sh
@@ -66,6 +66,8 @@ case $chosen in
 				bspc quit
 			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
 				i3-msg exit
+			elif [[ "$DESKTOP_SESSION" == "sway" ]]; then
+				swaymsg exit
 			fi
         ;;
 esac
