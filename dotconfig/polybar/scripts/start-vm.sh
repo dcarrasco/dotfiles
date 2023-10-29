4cfbf6e16de4f547bab3bbaf38b0e1424092c973
Daniel Carrasco <danielcarrasco17@gmail.com>
2023-10-29 00:26:24 -0300

    Comenta linea no usada

--- dotconfig/polybar/scripts/start-vm.sh
+++ dotconfig/polybar/scripts/start-vm.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 function main () {
     state=$(virsh --connect qemu:///system list --all | grep $1)
