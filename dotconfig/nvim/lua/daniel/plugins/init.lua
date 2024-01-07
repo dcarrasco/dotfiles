2d25071856f8802010a904bc732d9645e28a6260
Daniel Carrasco <danielcarrasco17@gmail.com>
2024-01-07 12:59:38 -0300

    Corrige inicio fugitive

--- dotconfig/nvim/lua/daniel/plugins/init.lua
+++ dotconfig/nvim/lua/daniel/plugins/init.lua
@@ -5,7 +5,7 @@ return {
         'tpope/vim-fugitive',
         config = function()
             vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-            vim.keymap.set("n", "<leader>gp", function() vim.cmd.Git 'push' end)
+            vim.keymap.set("n", "<leader>gp", function() vim.cmd('Git push') end)
         end
     },
     'tpope/vim-surround',
