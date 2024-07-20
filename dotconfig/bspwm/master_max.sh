43ffedf9c99930bf6a29ae512f45b3f7585bdb74
Daniel Carrasco <danielcarrasco17@gmail.com>
2024-07-20 17:11:53 -0400

    Actualiza bspwm, polybar

---
+++ dotconfig/bspwm/master_max.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+chk_cmd=$(bspc config single_monocle)
+
+# option=$(echo "$chk_cmd" | grep 'int' | awk '{print $2}')
+
+if [[ $chk_cmd == 'false' ]]; then
+    bspc config single_monocle true
+else
+    bspc config single_monocle false
+fi
+
+
--- dotconfig/polybar/scripts/bluetooth2.sh
+++ dotconfig/polybar/scripts/bluetooth2.sh
@@ -3,9 +3,11 @@ if [ $(bluetoothctl show | grep "Powered: yes" | wc -c) -eq 0 ]
 then
   echo "%{F#66ffffff}"
 else
-  if [ $(echo info | bluetoothctl | grep 'Device' | wc -c) -eq 0 ]
-  then
+  if [ $(echo info | bluetoothctl | grep 'Device ' | wc -c) -eq 0 ]
+ then
     echo ""
+  else
+    # echo "%{F#2193ff}"
+    echo " "
   fi
-  echo "%{F#2193ff}"
 fi
