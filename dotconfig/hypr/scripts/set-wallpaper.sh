d2b95b7bfa5634ccc0428da048d3cd31f8931ff7
Daniel Carrasco <danielcarrasco17@gmail.com>
2026-05-01 22:36:59 -0400

    Update mac style, set monocle on fullscreen

--- dotconfig/hypr/scripts/set-wallpaper.sh
+++ dotconfig/hypr/scripts/set-wallpaper.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-wallpaper="/home/daniel/Imágenes/fondos pantalla/nature/photo-1488226941561-6d7a806ae42a.jpg"
+wallpaper="/home/daniel/Imágenes/fondos pantalla/abstract/liquid/26-Tahoe-Dark.png"
 
 if [ -n "$1" ]; then
     wallpaper="$1"
