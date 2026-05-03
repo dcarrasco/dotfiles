dde0ff0674a9f3af346a8d44e944570c1175f3fa
Daniel Carrasco <danielcarrasco17@gmail.com>
2026-05-03 12:07:41 -0400

    Mejoras visuales

--- dotconfig/hypr/scripts/set-volume.sh
+++ dotconfig/hypr/scripts/set-volume.sh
@@ -7,14 +7,14 @@ if [ "$1" = "up" ]; then
     icon="audio-volume-high"
 elif [ "$1" = "down" ]; then
     step="5%-"
-    icon="audio-volume-high"
+    icon="audio-volume-low"
 elif [ "$1" = "toggle" ]; then
     amixer set Master toggle
     new_status=$(amixer get Master | grep "Front Left:" | sed -e "s/.*\[\(on\|off\)\].*/\1/")
     if [ "$new_status" = "off" ]; then
-        notify-send -c OSD -i audio-volume-muted "Volumen" "Mute"
+        notify-send -c OSD -i audio-volume-muted "Volumen Mute"
     else
-        notify-send -c OSD -i audio-volume-high "Volumen" "Unmute"
+        notify-send -c OSD -i audio-volume-high "Volumen Unmute"
     fi
 fi
 
--- dotconfig/hypr/scripts/show-keybinds.sh
+++ dotconfig/hypr/scripts/show-keybinds.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-rofi_theme=~/.config/rofi/themes/catppuccin-mocha.rasi
-
 if [ "$DESKTOP_SESSION" == "bspwm" ]; then
     keybinds=$(
         cat $HOME/.config/sxhkd/sxhkdrc |
@@ -27,16 +25,22 @@ else
         hyprctl binds -j |
         jq '.[] | [.modmask, .key, .description] | join(",")' |
         tr -d '"' |
-        sed 's/^64/ +/' |
-        sed 's/^65/ +[SHIFT]+/' |
-        sed 's/^68/ +[CTRL]+/' |
-        sed 's/^69/ +[CTRL]+[ALT]+/' |
-        sed 's/^72/ +[ALT]+/' |
-        sed 's/^73/ +[SHIFT]+[ALT]+/' |
+        sed 's/^64/ +/' |
+        sed 's/^65/ +[SHIFT]+/' |
+        sed 's/^68/ +[CTRL]+/' |
+        sed 's/^69/ +[CTRL]+[SHIFT]+/' |
+        sed 's/^72/ +[ALT]+/' |
+        sed 's/^73/ +[SHIFT]+[ALT]+/' |
         sed 's/^0//' |
         sed 's/^8/[ALT]+/' |
+        sed 's/mouse:272/[mouse left click]/' |
+        sed 's/mouse:273/[mouse right click]/' |
         awk -F, 'function rep(n,c){s=""; for(i=0;i<n;i++) s=s c; return s;} {print $1 $2 rep(30-length($1)-length($2), " ") $3}'
     )
 fi
 
-echo "$keybinds" | rofi -dmenu -i -p "Keybinds" -theme $rofi_theme
+theme_str="* {font: \"JetBrainsMono Nerd Font 12\";} window{width:40%; height:70%;}"
+echo "$keybinds" | rofi -dmenu -i -p "Keybinds" -theme-str "$theme_str"
+
+# rofi_theme="~/.config/rofi/themes/catppuccin-mocha.rasi"
+# echo "$keybinds" | rofi -dmenu -i -p "Keybinds" -theme $rofi_theme
