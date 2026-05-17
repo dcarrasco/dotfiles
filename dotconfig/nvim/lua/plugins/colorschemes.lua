9010e95357dc4d4e0fabd6a7a772042adc47baff
Daniel Carrasco <danielcarrasco17@gmail.com>
2026-05-17 10:15:17 -0400

    Fix transparent colors on vim nord theme

--- dotconfig/nvim/lua/plugins/colorschemes.lua
+++ dotconfig/nvim/lua/plugins/colorschemes.lua
@@ -68,7 +68,7 @@ return {
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
         -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
         -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
         -- vim.api.nvim_set_hl(0, "FloatTitle", { bg = "none" })
       end
